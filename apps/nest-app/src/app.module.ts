cbf87f8578006ab89d2d85ba06bcb851f12b1f32
miyou2024 <love@miyou.top>
2025-01-05 01:42:53 +0800

    test nest library

--- apps/nest-app/src/app.module.ts
+++ apps/nest-app/src/app.module.ts
@@ -1,9 +1,12 @@
+import { NestjsBootstrapModule } from '@ittlr/nestjs-bootstrap'
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    NestjsBootstrapModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
